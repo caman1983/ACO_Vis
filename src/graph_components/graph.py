ad3a272d59fee140983320a28cbcc6d214f51ca0
CamClark99 <CamClark1999@outlook.com>
2024-04-21 20:54:50 +0100

    Final commit.

--- src/graph_components/graph.py
+++ src/graph_components/graph.py
@@ -1,19 +1,19 @@
-
 from typing import Dict, Tuple, List
 from src.graph_components.node import Node
+
 
 class Graph:
     """
     Represents a graph data structure with nodes, edges, and pheromone levels for each edge.
     This class supports operations like adding nodes and edges, updating and evaporating pheromones,
-    and querying the graph structure for Ant Colony Optimization purposes.
+    and state handling for the graph structure.
     """
+
     def __init__(self):
         self.nodes_dict: Dict[str, Node] = {}
         self.edges_dict: Dict[Tuple[str, str], float] = {}
         self.pheromone_levels: Dict[Tuple[str, str], float] = {}
         self.default_pheromone_level = 1.0
-
 
     def add_node(self, node: Node) -> None:
         """
@@ -23,7 +23,6 @@ class Graph:
         node (Node): The node object to be added.
         """
         self.nodes_dict[node.id] = node
-
 
     def add_edge(self, start_nodeID: str, end_nodeID: str, distance: float) -> None:
         """
@@ -46,7 +45,6 @@ class Graph:
         else:
             raise Exception("One or both of the node ID's do not exist.")
 
-
     def update_pheromones(self, start_nodeID: str, end_nodeID: str, new_level: float) -> None:
         """
         Updates the pheromone level on an edge between two nodes.
@@ -60,7 +58,6 @@ class Graph:
         sorted_edges = tuple(sorted((start_nodeID, end_nodeID)))
         self.pheromone_levels[sorted_edges] += new_level
 
-
     def evaporate(self, evaporation_rate: float) -> None:
         """
         Applies evaporation to all pheromone levels in the graph.
@@ -70,7 +67,6 @@ class Graph:
         """
         for edge, level in self.pheromone_levels.items():
             self.pheromone_levels[edge] = level * (1 - evaporation_rate)
-
 
     def get_connected_nodes(self, node_id: str, prev_node_id: str) -> List[str]:
         """
@@ -91,7 +87,6 @@ class Graph:
                     connected_nodes.append(other_node)
         return connected_nodes
 
-
     def get_distance(self, edge: Tuple[str, str]) -> float:
         """
         Retrieves the distance or weight for a given edge.
@@ -104,7 +99,6 @@ class Graph:
         """
         sorted_edges = tuple(sorted(edge))
         return self.edges_dict.get(sorted_edges, 0.0)
-
 
     def get_pheromone_level(self, edge: Tuple[str, str]) -> float:
         """
@@ -128,12 +122,10 @@ class Graph:
         else:
             raise Exception("Error encountered attempting to retrieve node coordinates")
 
-
     def print_node_dict(self) -> None:
         print("Dictionary contains:", self.nodes_dict)
         print("Dictionary Keys:", self.nodes_dict.keys())
         print("Dictionary Values:", self.nodes_dict.values())
-
 
     def print_pheromone_levels(self):
         print("Pheromones:", self.pheromone_levels)
